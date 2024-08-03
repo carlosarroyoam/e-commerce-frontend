0d9fe50bd72370dd1edacecf3ab697a39611a206
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2024-08-03 17:59:08 -0600

    change app name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { AuthService } from './core/services/auth.service';
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  title = 'angular-auth';
+  title = 'Angular JWT/Auth';
 
   constructor(private readonly authService: AuthService) {}
 
