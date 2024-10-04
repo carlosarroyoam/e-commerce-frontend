6d3c76f3ed38a6afa17d37fb0dc38eaf2f996ccb
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2024-10-03 20:31:03 -0600

    add footer component

--- src/app/dashboard/dashboard-layout.component.ts
+++ src/app/dashboard/dashboard-layout.component.ts
@@ -2,10 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from '@/app/shared/components/header/header.component';
+import { FooterComponent } from '@/app/shared/components/footer/footer.component';
 
 @Component({
   standalone: true,
   templateUrl: './dashboard-layout.component.html',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  host: {
+    class: 'flex min-h-[100dvh] flex-col',
+  },
 })
 export class DashboardLayoutComponent {}
