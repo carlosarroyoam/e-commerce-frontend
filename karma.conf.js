31f10a1969be5aa49569073d0b94426a0b0d7921
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2024-10-20 22:29:10 -0600

    run prettier -w .

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,10 @@ module.exports = function (config) {
       suppressAll: true, // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require("path").join(__dirname, "./coverage/e-commerce-angular-frontend"),
+      dir: require("path").join(
+        __dirname,
+        "./coverage/e-commerce-angular-frontend",
+      ),
       subdir: ".",
       reporters: [{ type: "html" }, { type: "text-summary" }],
     },
