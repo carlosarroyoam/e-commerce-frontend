afa7722dc42c1cef90e5018880aaf2cfb0550f28
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2024-11-03 21:08:09 -0600

    add footer to auth layout

--- src/app/auth/auth-layout.component.ts
+++ src/app/auth/auth-layout.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { FooterComponent } from '@/app/shared/components/footer/footer.component';
+
 @Component({
   standalone: true,
   templateUrl: './auth-layout.component.html',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent],
+  host: {
+    class: 'flex min-h-[100dvh] flex-col',
+  },
 })
 export class AuthLayoutComponent {}
