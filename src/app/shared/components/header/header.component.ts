218a516676646d42aa89ef942f59e589337875e3
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2024-11-23 12:40:56 -0600

    add mobile styles, add mobile menu

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+import { AuthService } from '@/app/core/services/auth.service';
 import { UserNavComponent } from '@/app/shared/components/user-nav/user-nav.component';
 
 @Component({
@@ -9,4 +10,23 @@ import { UserNavComponent } from '@/app/shared/components/user-nav/user-nav.comp
   templateUrl: './header.component.html',
   imports: [RouterLink, UserNavComponent],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isMobileMenuOpen = false;
+  menuItems = [
+    { href: '/dashboard', title: 'Dashboard' },
+    { href: '/dashboard/orders', title: 'Orders' },
+    { href: '/dashboard/products', title: 'Products' },
+    { href: '/dashboard/categories', title: 'Categories' },
+    { href: '/dashboard/users', title: 'Users' },
+  ];
+
+  constructor(private readonly authService: AuthService) {}
+
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
+}
