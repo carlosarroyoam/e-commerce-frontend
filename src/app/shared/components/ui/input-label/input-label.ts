402b458068706a517fffbabb6fc1eddb3ac9b997
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2025-11-28 19:59:35 -0600

    add required indicator to labels

--- src/app/shared/components/ui/input-label/input-label.ts
+++ src/app/shared/components/ui/input-label/input-label.ts
@@ -1,4 +1,5 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, inject, input, OnInit } from '@angular/core';
+import { FormGroupDirective, Validators } from '@angular/forms';
 import { twMerge } from 'tailwind-merge';
 
 @Directive({
@@ -7,7 +8,20 @@ import { twMerge } from 'tailwind-merge';
     '[class]': 'hostClass',
   },
 })
-export class InputLabel {
+export class InputLabel implements OnInit {
+  private readonly formGroup = inject(FormGroupDirective);
+  private readonly elementRef = inject(ElementRef);
+
+  public for = input.required<string>();
+
+  ngOnInit(): void {
+    const control = this.formGroup.form.get(this.for());
+
+    if (control?.hasValidator(Validators.required)) {
+      this.elementRef.nativeElement.innerHTML += ` <span class="text-red-500">*</span>`;
+    }
+  }
+
   protected hostClass = twMerge(
     'block text-sm font-medium leading-6 text-zinc-900',
   );
