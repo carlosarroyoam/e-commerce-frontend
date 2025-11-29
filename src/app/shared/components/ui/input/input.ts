9963627f29725d6e2f14e5d19ab47af9db89e05f
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2025-11-28 21:23:04 -0600

    add invalid state styles

--- src/app/shared/components/ui/input/input.ts
+++ src/app/shared/components/ui/input/input.ts
@@ -1,14 +1,75 @@
-import { Directive } from '@angular/core';
+import {
+  AfterViewInit,
+  computed,
+  Directive,
+  inject,
+  OnDestroy,
+  OnInit,
+  signal,
+} from '@angular/core';
+import {
+  AbstractControl,
+  NgControl,
+  PristineChangeEvent,
+  TouchedChangeEvent,
+} from '@angular/forms';
+import { Subscription } from 'rxjs';
 import { twMerge } from 'tailwind-merge';
 
 @Directive({
   selector: 'input[appInput]',
   host: {
-    '[class]': 'hostClass',
+    '[class]': 'hostClass()',
   },
 })
-export class AppInput {
-  protected hostClass = twMerge(
-    'w-full rounded-md border-zinc-200 text-sm text-zinc-900 shadow-sm',
-  );
+export class AppInput implements OnInit, AfterViewInit, OnDestroy {
+  private readonly ngControl = inject(NgControl);
+  private control?: AbstractControl | null;
+  private changesSubscription = new Subscription();
+
+  private invalid = signal(false);
+
+  ngOnInit(): void {
+    this.control = this.ngControl?.control;
+  }
+
+  ngAfterViewInit(): void {
+    if (!this.control) return;
+
+    this.changesSubscription.add(
+      this.control.valueChanges.subscribe(() => this.isInvalid()),
+    );
+
+    this.changesSubscription.add(
+      this.control.events.subscribe((event) => {
+        if (
+          event instanceof TouchedChangeEvent ||
+          event instanceof PristineChangeEvent
+        ) {
+          this.isInvalid();
+        }
+      }),
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.changesSubscription.unsubscribe();
+  }
+
+  protected isInvalid(): void {
+    if (!this.control) return;
+
+    this.invalid.set(
+      this.control.invalid && (this.control.touched || this.control.dirty),
+    );
+  }
+
+  protected hostClass = computed(() => {
+    const invalid = this.invalid() && 'border-red-500';
+
+    return twMerge(
+      'w-full rounded-md border-zinc-200 text-sm text-zinc-900 shadow-sm',
+      invalid,
+    );
+  });
 }
