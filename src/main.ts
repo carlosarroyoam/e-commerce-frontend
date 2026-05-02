a28e7a18177d69d609ceb8c4ed3fb1db2fe8dbb4
Carlos Alberto Arroyo Martínez <carlosarroyoam@gmail.com>
2026-05-02 17:03:14 -0600

    remove @angular/localize

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-/// <reference types="@angular/localize" />
-
 import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 
